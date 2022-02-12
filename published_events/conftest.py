c7c212f08e9759699df08df797a65abf342da528
WanerValencia <waner.valencia@antpack.co>
2022-02-12 11:21:20 -0500

    first commit

---
+++ published_events/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from published_events.users.models import User
+from published_events.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
