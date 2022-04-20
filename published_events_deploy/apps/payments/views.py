e1be8e275f30fdaed531ec3a92c242cba0c77bf8
WanerValencia <waner.valencia@antpack.co>
2022-04-19 21:05:36 -0500

    change password and update profile

--- published_events_deploy/apps/payments/views.py
+++ published_events_deploy/apps/payments/views.py
@@ -1,5 +1,4 @@
 import hashlib
-import uuid
 
 from django.conf import settings
 from django.shortcuts import render
@@ -13,41 +12,44 @@ from django.core.exceptions import ObjectDoesNotExist
 
 from published_events_deploy.apps.transactions.actions import create_transaction
 from published_events_deploy.apps.events.models import TicketType
+
+
 class GeneratePaymentView(APIView):
     def post(self, request, *args, **kwargs):
         account_id = settings.PAYU_ACCOUNT_ID
         merchant_id = settings.PAYU_MERCHANT_ID
         api_key = settings.PAYU_API_KEY
         response_url = settings.PAYU_RESPONSE_URL
-        confirm_url = settings.PAYU_CONFIRM_URL        
-        
-        data:dict = request.data
+        confirm_url = settings.PAYU_CONFIRM_URL
+
+        data: dict = request.data
         email = data.get("email", None)
         buyer_full_name = data.get("full_name", None)
         phone = data.get("phone", None)
-        ticket_id:str= data.get("ticket_type_id", None)
-        ticket_quantity:int = data.get("ticket_quantity", None)
+        ticket_id: str = data.get("ticket_type_id", None)
+        ticket_quantity: int = data.get("ticket_quantity", None)
 
         if not email:
-            return Response({ "message": "email es requerido"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"message": "email es requerido"}, status=status.HTTP_400_BAD_REQUEST)
         elif not buyer_full_name:
-            return Response({ "message": "full_name es requerido"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"message": "full_name es requerido"}, status=status.HTTP_400_BAD_REQUEST)
         elif not phone:
-            return Response({ "message": "phone es requerido"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"message": "phone es requerido"}, status=status.HTTP_400_BAD_REQUEST)
         elif not ticket_id:
-            return Response({ "message": "ticket_type_id es requerido"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"message": "ticket_type_id es requerido"}, status=status.HTTP_400_BAD_REQUEST)
         elif not ticket_quantity:
-            return Response({ "message": "ticket_quantity es requerido"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"message": "ticket_quantity es requerido"}, status=status.HTTP_400_BAD_REQUEST)
 
         amount = None
-        ticket_type:TicketType = None
+        ticket_type: TicketType = None
         try:
             ticket_type = TicketType.objects.get(id=ticket_id)
             if not (ticket_type.availables - ticket_type.ticket_sales) >= ticket_quantity:
-                return Response({ "message": "La cantidad de entradas excede las permitidas"}, status=status.HTTP_400_BAD_REQUEST)
+                return Response({"message": "La cantidad de entradas excede las permitidas"},
+                                status=status.HTTP_400_BAD_REQUEST)
             amount = ticket_type.unit_price * ticket_quantity
         except ObjectDoesNotExist as ex:
-            return Response({ "message": "El ticket no es válido"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"message": "El ticket no es válido"}, status=status.HTTP_400_BAD_REQUEST)
 
         algorithm_signature = "MD5"
         currency = "COP"
@@ -69,7 +71,8 @@ class GeneratePaymentView(APIView):
         transaction = create_transaction("1128024080", ticket_type, 4, payment_result)
         print(transaction)"""
 
-        next_url = settings.API_LOCAL_BASE_URL + f"/payment/confirm?account_id={account_id}&merchant_id={merchant_id}&reference_code={reference_code}&amount={amount}&money={currency}&buyer_full_name={buyer_full_name}&email={email}&phone={phone}&test={test}&tax={tax}&tax_return_base={tax_return_base}&description={description}&payment_signature={payment_signature}&algorithm_signature={algorithm_signature}&confirm_url={confirm_url}&response_url={response_url}".replace(" ", "%20")
+        next_url = settings.API_LOCAL_BASE_URL + f"/payment/confirm?account_id={account_id}&merchant_id={merchant_id}&reference_code={reference_code}&amount={amount}&money={currency}&buyer_full_name={buyer_full_name}&email={email}&phone={phone}&test={test}&tax={tax}&tax_return_base={tax_return_base}&description={description}&payment_signature={payment_signature}&algorithm_signature={algorithm_signature}&confirm_url={confirm_url}&response_url={response_url}".replace(
+            " ", "%20")
 
         return Response({"message": "Orden creada con éxito", "next_url": next_url}, status=200)
 
@@ -89,14 +92,14 @@ class PaymentView(View):
                 }
         return render(request, 'payment/payment.html', context={"pay": data})
 
+
 class PaymentConfirmPayu(View):
-        def get(self, request, *args, **kwargs):
-         data = request.GET
-         return render(request, 'payment/payu_confirm.html', context={"pay": data})
+    def get(self, request, *args, **kwargs):
+        data = request.GET
+        return render(request, 'payment/payu_confirm.html', context={"pay": data})
+
 
 class PaymentResponsePayu(View):
-        def get(self, request, *args, **kwargs):
-         data = request.GET
-         return render(request, 'payment/payu_response.html', context={"pay": data})
-
-
+    def get(self, request, *args, **kwargs):
+        data = request.GET
+        return render(request, 'payment/payu_response.html', context={"pay": data})
--- published_events_deploy/apps/users/views.py
+++ published_events_deploy/apps/users/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate
 from rest_framework import status
 from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
@@ -9,6 +10,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 # custom serializers
+from .models import CustomUser
 from .serializers import RegisterSerializer, UserSerializer
 from ..utils import get_tokens_for_user
 
@@ -37,12 +39,12 @@ class UserView(ViewSet):
     def get_queryset(self):
         return self.request.user
 
-    @action(detail=False, methods=["GET"], name="profile", url_path="profile")
+    @action(detail=False, methods=["GET"], name="profile", url_path="profile", url_name="profile")
     def profile(self, request):
         user = self.get_queryset()
         serializer = UserSerializer(user, context={"request": request})
-        return Response({"data": { "user": serializer.data}}, status.HTTP_200_OK)
-    
+        return Response({"data": {"user": serializer.data}}, status.HTTP_200_OK)
+
     @action(detail=False, methods=["POST"], name="update_image_profile", url_path="update_image_profile")
     def update_image_profile(self, request, *args, **kwargs):
         files = self.request.FILES
@@ -53,18 +55,75 @@ class UserView(ViewSet):
             if not image:
                 return Response({"image": ["Este campo es requerido"]}, status=status.HTTP_400_BAD_REQUEST)
 
-            if image.size > 1024000: 
-                return Response({"message": ["El tamaño del archivo excede el maximo permitido(1MB)"]}, status=status.HTTP_400_BAD_REQUEST)
+            if image.size > 1024000:
+                return Response({"message": ["El tamaño del archivo excede el maximo permitido(1MB)"]},
+                                status=status.HTTP_400_BAD_REQUEST)
 
             if not image.content_type.split("/")[0] == "image":
-                return Response({"message": ["Imagen no válida, revisa el formato del archivo"]}, status=status.HTTP_400_BAD_REQUEST)
+                return Response({"message": ["Imagen no válida, revisa el formato del archivo"]},
+                                status=status.HTTP_400_BAD_REQUEST)
 
             user.image = image
             user.save()
             user_serialized = UserSerializer(user, context={"request": request}).data
 
-            return Response({"data": {"message": "Imagen actualiza correctamente", "image": user_serialized.get("image")}}, status=status.HTTP_200_OK)
+            return Response(
+                {"data": {"message": "Imagen actualiza correctamente", "image": user_serialized.get("image")}},
+                status=status.HTTP_200_OK)
 
         except Exception as e:
             print(e)
-            return Response({"message": "Error al actualizar la imagen, intenta más tarde"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"message": "Error al actualizar la imagen, intenta más tarde"},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=False, methods=["PUT", "POST"], name="update profile", url_path="update_profile")
+    def update_user_profile(self, request, *args, **kwargs):
+        user: CustomUser = self.get_queryset()
+        data: dict = request.data
+        if data.get("identification"):
+            identification_exist = CustomUser.objects.filter(
+                identification__exact=data.get("identification")).distinct().exists()
+            if identification_exist:
+                return Response({"message": ["La identificación no es válida"]}, status.HTTP_400_BAD_REQUEST)
+            user.identification = data.get("identification")
+        elif data.get("description"):
+            user.description = data.get("description")
+        elif data.get("first_name"):
+            user.description = data.get("first_name")
+        elif data.get("last_name"):
+            user.description = data.get("last_name")
+
+        user.save()
+        user_data = UserSerializer(user, context={"request": request}).data
+
+        return Response({"data": {"user": user_data}}, status.HTTP_200_OK)
+
+    @action(detail=False, methods=["POST"], name="change profile", url_path="change_password")
+    def change_password(self, request, *args, **kwargs):
+        user: CustomUser = self.get_queryset()
+        data: dict = request.data
+
+        old_password = data.get("old_password", None)
+        new_password = data.get("new_password", None)
+        confirm_password = data.get("confirm_password", None)
+
+        if not old_password:
+            return Response({"message": ["old_password es requerido"]}, status=status.HTTP_400_BAD_REQUEST)
+
+        elif not new_password:
+            return Response({"message": ["new_password es requerido"]}, status=status.HTTP_400_BAD_REQUEST)
+
+        elif not confirm_password:
+            return Response({"message": ["confirm_password es requerido"]}, status=status.HTTP_400_BAD_REQUEST)
+
+        if not new_password == confirm_password:
+            return Response({"message": ["La nueva contraseñas debe coincidir con la confirmación"]}, status=status.HTTP_400_BAD_REQUEST)
+
+        if not user.check_password(old_password):
+            return Response({"message": ["La contraseña anterior no es correcta"]},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        user.set_password(new_password)
+        user.save()
+
+        return Response({"message": ["Contraseña actualizada correctamente"]}, status=status.HTTP_200_OK)
