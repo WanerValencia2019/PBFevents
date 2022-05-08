1cc7d62145ace2bc8a71a3e552c31506e393afe9
WanerValencia <waner.valencia@antpack.co>
2022-05-07 19:57:30 -0500

    changes

--- published_events_deploy/apps/payments/urls.py
+++ published_events_deploy/apps/payments/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from published_events_deploy.apps.payments.views import PaymentView, GeneratePaymentView
+from published_events_deploy.apps.payments.views import PaymentConfirmPayu, PaymentView, GeneratePaymentView
 
 urlpatterns = [
     path('payment', GeneratePaymentView.as_view(), name="payment"),
     path("payment/confirm", PaymentView.as_view(), name="payment_confirm"),
-    path("payment/payu/confirm", PaymentView.as_view(), name="payment_payu_confirm"),
-    path("payment/payu/response", PaymentView.as_view(), name="payment_payu_response"),
+    path("payment/payu/confirm", PaymentConfirmPayu.as_view(), name="payment_payu_confirm"),
+    path("payment/payu/response", PaymentConfirmPayu.as_view(), name="payment_payu_response"),
 ]
